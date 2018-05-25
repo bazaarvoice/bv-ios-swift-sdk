de29bca93f6ba62d9bb27233ce881355ec6ab6a7
Michael Van Milligan <michael.vanmilligan@bazaarvoice.com>
2018-05-25 13:28:54 -0500

    Adding comments to publicly facing interfaces for BVCommon and BVAnalytics.

--- Sources/BVSwift/BVCommon/BVManager.swift
+++ Sources/BVSwift/BVCommon/BVManager.swift
@@ -7,6 +7,17 @@
 
 import Foundation
 
+/// The highest abstraction class of this module. This singleton class'
+/// intention is to handle all the top level configuration and state management.
+/// - Important:
+/// \
+/// Most of the objects that the BVManager interoperate with don't necessarily
+/// need help from it, however, if you don't configure those objects then they
+/// will likely fail. Therefore, if you include a configuration file in your
+/// project this class will parse it and register the various configurations
+/// within itself. These configurations can then be picked up by the BVSwift
+/// objects directly by calling through the respective BVManager hooks defined
+/// mostly in the extensions provided in each of the submodules.
 public class BVManager {
   
   private static var configFileProduction: String = "bvsdk_config_prod"
@@ -47,9 +58,10 @@ public class BVManager {
   
   private init() {}
   
-  /// Public
+  /// Singleton interface
   public static let sharedManager = BVManager()
   
+  /// The URLSession object to be used in any networking operations
   public var urlSession: URLSession {
     get {
       return internalURLSession
@@ -59,6 +71,7 @@ public class BVManager {
     }
   }
   
+  /// The log level to be used globally
   public var logLevel: BVLogger.BVLogLevel {
     get {
       return BVLogger.sharedLogger.logLevel
@@ -68,6 +81,22 @@ public class BVManager {
     }
   }
   
+  /// Interface to add configurations to the BVManager
+  /// - Parameters:
+  ///   - configuration: The BVConfiguration to be added to this manager
+  ///
+  /// - Note:
+  /// \
+  /// You can only have one type of configuration enqueued at any one time.
+  ///
+  /// - Important:
+  /// \
+  /// This function should be used if using solely programmatic configuration
+  /// of the various used modules or to augment the configurations set into the
+  /// the file. If any configuration is added programmatically it *will*
+  /// overwrite anything in the configuration file. This is an added *feature*
+  /// so that it's easier to fluidly and dynamically move from staging to
+  /// production and to do so with the granularity of each submodule of BVSwift.
   @discardableResult
   public func addConfiguration(
     _ configuration: BVConfiguration) -> Self {
@@ -85,6 +114,10 @@ public class BVManager {
   }
 }
 
+/// Generic function to search and return a given BVConfiguration. As the
+/// BVManager.addConfiguration describes: there can only be one singular type
+/// of configuration and it defaults to any that are programmatically added
+/// before switching to what is in the configuration file.
 internal extension BVManager {
   func getConfiguration<T: BVConfiguration>() -> T? {
     
