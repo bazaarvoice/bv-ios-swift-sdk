b69289c16fb25c90084d5c1355151960ea0a2d96
Michael Van Milligan <michael.vanmilligan@bazaarvoice.com>
2018-06-01 14:21:22 -0500

    Adding test for logger redirection.

--- Tests/BVSwiftTests/Common/BVCommonTest.swift
+++ Tests/BVSwiftTests/Common/BVCommonTest.swift
@@ -7,3 +7,48 @@
 // 
 
 import Foundation
+
+import XCTest
+@testable import BVSwift
+
+class BVCommonTest: XCTestCase {
+  
+  private static var analyticsConfig: BVAnalyticsConfiguration = {
+    return .configuration(
+      locale: Locale.autoupdatingCurrent,
+      configType: .staging(clientId: "conciergeapidocumentation"))
+  }()
+  
+  private static var privateSession: URLSession = {
+    return URLSession(configuration: .default)
+  }()
+  
+  override func setUp() {
+    super.setUp()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  func testAnalyticsEventAppStateSubmission() {
+    
+    let expectation =
+      self.expectation(description: "testAnalyticsEventAppStateSubmission")
+    
+    BVLogger.sharedLogger.logLevel = .debug
+    BVLogger.sharedLogger.loggerRedirect = { (msg: String) in
+      print(msg)
+      expectation.fulfill()
+    }
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1000)) {
+      BVLogger.sharedLogger.debug("Fullfilling expectation")
+    }
+    
+    self.waitForExpectations(timeout: 20) { (error) in
+      XCTAssertNil(
+        error, "Something went horribly wrong, request took too long.")
+    }
+  }
+}
