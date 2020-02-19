7f37ecf64ebb44f45629ccdcd2a395be28f2b0f1
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-02-19 20:12:01 +0530

    Added ReviewHighlights constants.

--- Sources/BVSwift/BVConversations/ReviewHighlights/BVReviewHighlightsConstants.swift
+++ Sources/BVSwift/BVConversations/ReviewHighlights/BVReviewHighlightsConstants.swift
@@ -7,3 +7,15 @@
 // 
 
 import Foundation
+
+internal struct BVReviewHighlightsConstants {
+    
+    static let stagingEndpoint: String = ""
+    static let productionEndpoint: String = "https://rh.nexus.bazaarvoice.com/"
+    
+    internal struct BVReviewHighlights {
+        static let singularKey: String = "subjects"
+        static let pluralKey: String = "subjects"
+        static let getResource: String = "highlights/v3/1/"
+    }
+}
