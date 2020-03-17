a31f69fcb9c9b43722101ec37f9d506b9ed01671
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-03-17 14:18:48 +0530

    Changed some access specifiers and updated the manager implementation.

--- BVSwift/BVConversations/ReviewHighlights/Display/Types/BVProductReviewHighlightsQuery.swift
+++ BVSwift/BVConversations/ReviewHighlights/Display/Types/BVProductReviewHighlightsQuery.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class BVProductReviewHighlightsQuery: BVReviewHighlightsQuery<BVReviewHighlights> {
+public class BVProductReviewHighlightsQuery: BVReviewHighlightsQuery<BVReviewHighlights> {
     
     public var productId: String?
     
