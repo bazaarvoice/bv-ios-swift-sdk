6924cbfa30a1ed0aa17c883269bc319d3e8ae440
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-03-18 10:52:17 +0530

    Added test case for Review Highlights Manager implementation.

--- BVSwiftTests/BVConversations/Display/BVReviewHighlightsQueryTest.swift
+++ BVSwiftTests/BVConversations/Display/BVReviewHighlightsQueryTest.swift
@@ -30,6 +30,71 @@ class BVReviewHighlightsQueryTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
+    func testManagerImplementationForReviewHighlightsQuery() {
+        
+        let expectation = self.expectation(description: "testReviewHighlights")
+        
+        BVManager.sharedManager.addConfiguration(BVReviewHighlightsQueryTest.config)
+        
+        guard let reviewHighlightsQuery = BVManager.sharedManager.query(clientId: "1800petmeds", productId: "prod1011") else {
+            XCTFail()
+            expectation.fulfill()
+            return
+        }
+        
+        reviewHighlightsQuery.handler { (response: BVReviewHighlightsQueryResponse<BVReviewHighlights>) in
+            
+            if case .failure(let error) = response {
+                print(error)
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            guard case let .success(reviewHighlights) = response else {
+                XCTFail()
+                expectation.fulfill()
+                return
+            }
+            
+            XCTAssertNotNil(reviewHighlights.positives)
+            if let positives = reviewHighlights.positives {
+                XCTAssertFalse(positives.isEmpty)
+                
+                for positive in positives {
+                    XCTAssertNotNil(positive.title)
+                }
+            }
+            
+            XCTAssertNotNil(reviewHighlights.negatives)
+            if let negatives = reviewHighlights.negatives {
+                XCTAssertFalse(negatives.isEmpty)
+                
+                for negative in negatives {
+                    XCTAssertNotNil(negative.title)
+                }
+            }
+
+            expectation.fulfill()
+        }
+        
+        guard let req = reviewHighlightsQuery.request else {
+            XCTFail()
+            expectation.fulfill()
+            return
+        }
+        
+        print(req)
+        
+        reviewHighlightsQuery.async(urlSession: BVReviewHighlightsQueryTest.privateSession)
+        
+        self.waitForExpectations(timeout: 10) { (error) in
+            XCTAssertNil(
+                error, "Something went horribly wrong, request took too long.")
+        }
+        
+    }
+    
     func testReviewHighlights() {
         
         let expectation = self.expectation(description: "testReviewHighlights")
