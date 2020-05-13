2a94d8e46c95f0959082903074d3f86709ae9fa8
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-13 16:05:39 +0530

    Review Highlights analytics tracking implementation.

--- BVSwift/BVConversations/ReviewHighlights/Display/Types/BVProductReviewHighlightsQuery.swift
+++ BVSwift/BVConversations/ReviewHighlights/Display/Types/BVProductReviewHighlightsQuery.swift
@@ -11,9 +11,32 @@ import Foundation
 /// Public class for handling BVProductReviewHighlights Queries
 public class BVProductReviewHighlightsQuery: BVReviewHighlightsQuery<BVReviewHighlights> {
         
+    public let productId: String?
+    
+    override var reviewHighlightsPostflightResultsClosure: ((BVReviewHighlights?) -> Void)? {
+        return { [weak self] (reviewHighlights: BVReviewHighlights?) in
+            if nil != reviewHighlights,
+              let productId = self?.productId {
+              let reviewHighlightsFeatureEvent: BVAnalyticsEvent =
+                .feature(
+                  bvProduct: .reviews,
+                  name: .reviewHighlights,
+                  productId: productId,
+                  brand: nil,
+                  additional: nil)
+                
+              BVPixel.track(
+                reviewHighlightsFeatureEvent,
+                analyticConfiguration: self?.configuration?.analyticsConfiguration)
+            }
+        }
+    }
+    
     public init(clientId: String, productId: String) {
 
         BVReviewHighlights.set(clientId: clientId, productId: productId)
+        
+        self.productId = productId
         
         super.init(BVReviewHighlights.self)
     }
