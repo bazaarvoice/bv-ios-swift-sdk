e9b40a4b40aea82f377c5baabaaecf12f235ad23
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-19 14:58:44 +0530

    Created base protocols and initializing coordinator.

--- Examples/BVSwiftDemo/BVSwiftDemo/AppDelegate.swift
+++ Examples/BVSwiftDemo/BVSwiftDemo/AppDelegate.swift
@@ -11,10 +11,20 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var coordinator: AppCoordinator?
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let navController = UINavigationController()
+        coordinator = AppCoordinator(navigationController: navController)
+        coordinator?.start()
+
+        // create a basic UIWindow and activate it
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
         return true
     }
 
