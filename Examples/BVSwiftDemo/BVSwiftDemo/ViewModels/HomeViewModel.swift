9d5aa514b1676235f722bdd6b8978e97e3049e69
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-19 16:46:10 +0530

    Implemented navigation base protocols.

---
+++ Examples/BVSwiftDemo/BVSwiftDemo/ViewModels/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  BVSwiftDemo
+//
+//  Created by Balkrishna Singbal on 19/05/20.
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+}
