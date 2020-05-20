833e3df37f2ad6566aae8d0581db5a844d272fc1
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-20 10:51:27 +0530

    Navigation Bar configuration.

---
+++ Examples/BVSwiftDemo/BVSwiftDemo/Utils.swift
@@ -0,0 +1,16 @@
+//
+//  Utils.swift
+//  BVSwiftDemo
+//
+//  Created by Balkrishna Singbal on 20/05/20.
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static func bazaarvoiceNavy() -> UIColor {
+      return #colorLiteral(red: 0, green: 0.2392156863, blue: 0.3019607843, alpha: 1)
+    }
+}
