0a5720336d6b3a3d1930330bfbff5040909f481e
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-20 10:18:44 +0530

    Created ConversationsAPIList view controller and view model.

---
+++ Examples/BVSwiftDemo/BVSwiftDemo/ViewModels/ConversationsAPIListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ConversationsAPIListViewModel.swift
+//  BVSwiftDemo
+//
+//  Created by Balkrishna Singbal on 20/05/20.
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+//
+
+import Foundation
+
+class ConversationsAPIListViewModel {
+    
+}
