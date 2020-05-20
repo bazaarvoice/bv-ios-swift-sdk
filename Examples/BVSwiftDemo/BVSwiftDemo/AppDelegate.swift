fd8b2c6110a3bdc5021f132060184b501e4d1233
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-20 10:51:27 +0530

    Navigation Bar configuration.

--- Examples/BVSwiftDemo/BVSwiftDemo/AppDelegate.swift
+++ Examples/BVSwiftDemo/BVSwiftDemo/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let navController = UINavigationController()
+        let navController = BVSwiftDemoNavigationController()
         coordinator = AppCoordinator(navigationController: navController)
         coordinator?.start()
 
--- Examples/BVSwiftDemo/BVSwiftDemo/ViewControllers/HomeViewController.swift
+++ Examples/BVSwiftDemo/BVSwiftDemo/ViewControllers/HomeViewController.swift
@@ -17,6 +17,15 @@ class HomeViewController: UIViewController, Storyboarded {
         // Do any additional setup after loading the view.
     }
 
+    class func createTitleLabel() -> UILabel {
+        let titleLabel = UILabel(frame: CGRect(x: 0,y: 0,width: 200,height: 44))
+        
+        titleLabel.text = "bazaarvoice:";
+        titleLabel.textColor = UIColor.white
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont(name: "ForalPro-Regular", size: 36)
+        return titleLabel
+    }
 
 }
 
