27baf047528c08139225076c3d2496635a07f107
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-20 10:18:44 +0530

    Created ConversationsAPIList view controller and view model.

---
+++ Examples/BVSwiftDemo/BVSwiftDemo/ViewControllers/ConversationsAPIListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConversationsAPIListViewController.swift
+//  BVSwiftDemo
+//
+//  Created by Balkrishna Singbal on 20/05/20.
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsAPIListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
