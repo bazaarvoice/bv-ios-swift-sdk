cc8eb255c435b14e3704266774a99f3659112627
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-21 15:19:44 +0530

    Added method to present alert in coordinator.

--- Examples/BVSwiftDemo/BVSwiftDemo/Utils.swift
+++ Examples/BVSwiftDemo/BVSwiftDemo/Utils.swift
@@ -11,11 +11,11 @@ import UIKit
 
 extension UIColor {
     
-    class var bazaarvoiceNavy: UIColor { return #colorLiteral(red: 0, green: 0.2392156863, blue: 0.3019607843, alpha: 1) }
+    class var bazaarvoiceNavy : UIColor { return #colorLiteral(red: 0, green: 0.2392156863, blue: 0.3019607843, alpha: 1) }
     
-    class var bazaarvoiceTeal: UIColor { return #colorLiteral(red: 0, green: 0.6705882353, blue: 0.5607843137, alpha: 1) }
+    class var bazaarvoiceTeal : UIColor { return #colorLiteral(red: 0, green: 0.6705882353, blue: 0.5607843137, alpha: 1) }
     
-    class var bazaarvoiceGold: UIColor { return #colorLiteral(red: 0.9882352941, green: 0.7176470588, blue: 0.06666666667, alpha: 1) }
+    class var bazaarvoiceGold : UIColor { return #colorLiteral(red: 0.9882352941, green: 0.7176470588, blue: 0.06666666667, alpha: 1) }
     
-    class var appBackground: UIColor   { return #colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9607843137, alpha: 1) }
+    class var appBackground   : UIColor { return #colorLiteral(red: 0.9607843137, green: 0.9607843137, blue: 0.9607843137, alpha: 1) }
 }
