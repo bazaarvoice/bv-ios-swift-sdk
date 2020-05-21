96b7e91a46b2cb83b242c3fa202b963989765284
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-21 12:39:41 +0530

    Refactored Coordinator implementation.

--- Examples/BVSwiftDemo/BVSwiftDemo/Coordinators/Coordinator.swift
+++ Examples/BVSwiftDemo/BVSwiftDemo/Coordinators/Coordinator.swift
@@ -9,17 +9,57 @@
 import Foundation
 import UIKit
 
-protocol Coordinator: class {
-    
-    var childCoordinators: [Coordinator] { get set }
-    
-    var navigationController: UINavigationController { get set }
-    
-    func start()
-    
-    func navigateTo(_ scene: AppNavigator)
-}
 
 protocol AppNavigator {
     
+}
+
+
+class Coordinator: NSObject, UINavigationControllerDelegate {
+    
+    var childCoordinators: [Coordinator] = [Coordinator]()
+    
+    var navigationController: UINavigationController
+    
+    var parentCoordinator: Coordinator?
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        self.navigationController.delegate = self
+    }
+    
+    func navigateTo(_ scene: AppNavigator) {
+        
+    }
+    
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+
+        guard let fromViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else {
+            return
+        }
+
+        if navigationController.viewControllers.contains(fromViewController) {
+            // Its a push; do nothing
+            return
+        }
+
+        // Its a pop. Check if it is the coordinator's first view controller and call parent coordinators childDidFinish() method.
+        if fromViewController is ConversationsAPIListViewController {
+            self.parentCoordinator?.childDidFinish(self)
+        }
+
+    }
+
+    func childDidFinish(_ child: Coordinator?) {
+        for (index, coordinator) in self.childCoordinators.enumerated() {
+            if coordinator === child {
+                childCoordinators.remove(at: index)
+                self.navigationController.delegate = self
+                break
+            }
+        }
+    }
 }
