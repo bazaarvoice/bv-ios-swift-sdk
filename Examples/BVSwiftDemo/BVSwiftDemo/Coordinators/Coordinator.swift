76e40992176b6bceec176594a743c006b1155700
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-21 15:19:44 +0530

    Added method to present alert in coordinator.

--- Examples/BVSwiftDemo/BVSwiftDemo/Coordinators/Coordinator.swift
+++ Examples/BVSwiftDemo/BVSwiftDemo/Coordinators/Coordinator.swift
@@ -42,6 +42,14 @@ class Coordinator: NSObject, UINavigationControllerDelegate {
         }
     }
     
+    func showAlert(title: String, message: String, handler: (() -> Void)?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Okay", style: .default, handler: { (action) in
+            handler?()
+        }))
+        self.navigationController.present(alertController, animated: true, completion: nil)
+    }
+    
     // MARK:- UINavigationControllerDelegate methods
     func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
 
