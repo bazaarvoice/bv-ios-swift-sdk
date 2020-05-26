f53423f045cf6637e9b60feebcd3efcbf366397c
Abhinav Mandloi <abhinav.mandloi@bazaarvoice.com>
2020-05-26 10:21:26 +0530

    Made BVSwift configuration changes.

--- Examples/BVSwiftDemo/BVSwiftDemo/Scheme Configuration/Config.swift
+++ Examples/BVSwiftDemo/BVSwiftDemo/Scheme Configuration/Config.swift
@@ -7,48 +7,49 @@
 //
 
 import UIKit
+import BVSwift
 
-enum ConfigTypes: String {
-
-    case Staging
-    case Production
+enum Environment: String {
+    case staging
+    case production
 }
 
 class Config: NSObject {
-
+    
     static let sharedInstance = Config()
-
-    private var configs: NSDictionary = [:]
-    private var currentConfig: String = ""
-    private let errorStr: String = "ERROR"
-
+    private let environment: Environment
+    private let clientId: String = "1800petmeds"
+    private let conversationsPassKey: String = "1800petmeds"
+    var config: BVConversationsConfiguration?
+    
+    
     private override init() {
-
-        if let currentConfigStr = Bundle.main.object(forInfoDictionaryKey: "Config") as? String {
-
-            self.currentConfig = currentConfigStr
-
-            switch currentConfigStr.lowercased() {
-
-            case ConfigTypes.Production.rawValue.lowercased() :
-
-                if let path = Bundle.main.path(forResource: "Prod", ofType: "plist") {
-                    self.configs = NSDictionary(contentsOfFile: path)!
-                }
-
-            case ConfigTypes.Staging.rawValue.lowercased() :
-                if let path = Bundle.main.path(forResource: "QA", ofType: "plist") {
-                    self.configs = NSDictionary(contentsOfFile: path)!
-                }
-
-            default  :
-                if let path = Bundle.main.path(forResource: "Dev", ofType: "plist") {
-                    self.configs = NSDictionary(contentsOfFile: path)!
-                }
-
-            } //end of case
+        
+        if let currentConfigStr = Bundle.main.object(forInfoDictionaryKey: "ENV") as? String, let env = Environment.init(rawValue: currentConfigStr)  {
+            
+            self.environment = env
+            
         }
-
+        else {
+            self.environment = .staging
+        }
+        
+        super.init()
     } //end of init
-
+    
+    
+    func setConfiguration() {
+        switch self.environment {
+            
+        case .staging :
+            let analyticsConfig: BVAnalyticsConfiguration =  .dryRun(configType: .staging(clientId: self.clientId))
+            self.config = BVConversationsConfiguration.all(clientKey: self.conversationsPassKey, configType: .staging(clientId: self.clientId), analyticsConfig: analyticsConfig)
+            
+        case .production :
+            let analyticsConfig: BVAnalyticsConfiguration =  .dryRun(configType: .production(clientId: self.clientId))
+            self.config = BVConversationsConfiguration.all(clientKey: self.conversationsPassKey, configType: .production(clientId: self.clientId), analyticsConfig: analyticsConfig)
+            
+        } //end of case
+    }
+    
 } //end of class
