0ed9b20a625d9eaf02de2f976027ad4cecd7f41d
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-27 13:01:05 +0530

    Created ConversationsSubmissionAPI model.

--- Examples/BVSwiftDemo/BVSwiftDemo/Models/ConversationsDisplayAPI.swift
+++ Examples/BVSwiftDemo/BVSwiftDemo/Models/ConversationsDisplayAPI.swift
@@ -68,47 +68,4 @@ enum ConversationsDisplayAPI: Int, CaseIterable, AppNavigator {
             
         }
     }
-    
-}
-
-enum ConversationsSubmissionAPI: Int, CaseIterable, AppNavigator {
-    
-    case answerSubmission
-    
-    case commentSubmission
-    
-    case feedbackSubmission
-    
-    case photoSubmission
-    
-    case questionSubmission
-    
-    case reviewSubmission
-    
-    case uasSubmission
-    
-    case progressiveSubmission
-    
-    var titleText: String {
-        
-        switch self {
-            
-        case .answerSubmission: return "Answer Submission"
-            
-        case .commentSubmission: return "Comment Submission"
-            
-        case .feedbackSubmission: return "Feedback Submission"
-            
-        case .photoSubmission: return "Photo Submission"
-            
-        case .questionSubmission: return "Question Submission"
-            
-        case .reviewSubmission: return "Review Submission"
-            
-        case .uasSubmission: return "UAS Submission"
-            
-        case .progressiveSubmission: return "Progressive Submission"
-        }
-    }
-    
 }
