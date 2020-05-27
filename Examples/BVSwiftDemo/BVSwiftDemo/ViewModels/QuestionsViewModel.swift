499f5510998670fad0c00ea0c9f7367589c8eb75
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-27 15:57:57 +0530

    Added ViewController and ViewModel for Questions list screen.

---
+++ Examples/BVSwiftDemo/BVSwiftDemo/ViewModels/QuestionsViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  QuestionsViewModel.swift
+//  BVSwiftDemo
+//
+//  Created by Balkrishna Singbal on 27/05/20.
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+//
+
+import Foundation
+
+protocol QuestionsViewModelDelegate: class {
+    
+}
+
+class QuestionsViewModel: ViewModelType {
+    
+    weak var viewController: ConversationsAPIListViewControllerDelegate?
+    
+    weak var coordinator: Coordinator?
+}
+
+// MARK:- QuestionsViewModelDelegate
+extension QuestionsViewModel: QuestionsViewModelDelegate {
+    
+}
