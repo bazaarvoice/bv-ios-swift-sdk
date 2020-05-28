df05776fe2949477c95a638f46077c4e82095ee4
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-05-28 12:09:38 +0530

    Fetch Questions API Integration.

--- Examples/BVSwiftDemo/BVSwiftDemo/ViewModels/QuestionsViewModel.swift
+++ Examples/BVSwiftDemo/BVSwiftDemo/ViewModels/QuestionsViewModel.swift
@@ -7,9 +7,10 @@
 //
 
 import Foundation
+import BVSwift
 
 protocol QuestionsViewModelDelegate: class {
-    
+    func fetchQuestions()
 }
 
 class QuestionsViewModel: ViewModelType {
@@ -17,9 +18,35 @@ class QuestionsViewModel: ViewModelType {
     weak var viewController: QuestionsTableViewControllerDelegate?
     
     weak var coordinator: Coordinator?
+    
+    private var questions: [BVQuestion]?
 }
 
 // MARK:- QuestionsViewModelDelegate
 extension QuestionsViewModel: QuestionsViewModelDelegate {
     
+    func fetchQuestions() {
+        
+        let questionQuery = BVQuestionQuery(productId: "test1",
+                                            limit: 10,
+                                            offset: 0)
+            .configure(Config.sharedInstance.config)
+            .handler { (response: BVConversationsQueryResponse<BVQuestion>) in
+                
+                if case .failure(let error) = response {
+                  print(error)
+                    // TODO:- show alert
+                  return
+                }
+                
+                guard case let .success(_, questions) = response else {
+                    // TODO:- show alert
+                  return
+                }
+                
+                self.questions = questions
+        }
+        
+        questionQuery.async()
+    }
 }
