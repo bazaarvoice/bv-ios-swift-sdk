227c83831c4ce3c6bd4ac3844d452e3486a7f6a5
Abhinav Mandloi <abhinav.mandloi@bazaarvoice.com>
2020-05-28 11:14:39 +0530

    Added Navigation and Business Logic Implementation.

--- Examples/BVSwiftDemo/BVSwiftDemo/ViewControllers/HomeViewController.swift
+++ Examples/BVSwiftDemo/BVSwiftDemo/ViewControllers/HomeViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class HomeViewController: UIViewController, ViewControllerType {
     
+    // MARK:- IBOutlets
+    @IBOutlet weak var bvModulesTableView: UITableView!
+    
+    // MARK:- Constants
+    private static let CELL_IDENTIFIER = "BVModuleListCell"
+    
+    // MARK:- Variables
     var viewModel: HomeViewModelDelegate!
     
+    // MARK:- Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.bvModulesTableView.tableFooterView = UIView()
     }
-
+    
     class func createTitleLabel() -> UILabel {
         let titleLabel = UILabel(frame: CGRect(x: 0,y: 0,width: 200,height: 44))
         
@@ -28,9 +38,41 @@ class HomeViewController: UIViewController, ViewControllerType {
     }
     
     @IBAction func showNextTapped(_ sender: Any) {
-        self.viewModel.didTapShowNextButton()
+       // self.viewModel.didTapShowNextButton()
     }
     
+    
+}
 
+// Mark:- HomeViewControllerDatasource methods
+extension HomeViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.viewModel.numberOfSections
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.viewModel.numberOfRows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeViewController.CELL_IDENTIFIER) else {
+            return UITableViewCell()
+        }
+        
+        cell.selectionStyle = .none
+        cell.textLabel?.text = self.viewModel.titleForRowAtIndexPath(indexPath)
+
+        return cell
+    }
+}
+
+// MARK:- HomeViewControllerDelegate methods
+extension HomeViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       self.viewModel.didSelectRowAt(indexPath: indexPath)
+    }
 }
 
