dd904d0ae5f4242936846bc424c27e4ba42bf116
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-06-02 12:40:42 +0530

    Added ViewModel and ViewController classes for Product Display Page. Integrated HCSStarRatingView pod.

---
+++ Examples/BVSwiftDemo/BVSwiftDemo/ViewModels/ProductDisplayPageViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProductDisplayPageViewModel.swift
+//  BVSwiftDemo
+//
+//  Created by Balkrishna Singbal on 02/06/20.
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+//
+
+import Foundation
+
+class ProductDisplayPageViewModel {
+    
+}
