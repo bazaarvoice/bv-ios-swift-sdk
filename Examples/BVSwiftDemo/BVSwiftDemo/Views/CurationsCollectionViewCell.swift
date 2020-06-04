b3685b01068bed1c302ee7a454aff73e172acac1
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-06-04 21:27:49 +0530

    Curations View Deisgn.

---
+++ Examples/BVSwiftDemo/BVSwiftDemo/Views/CurationsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CurationsCollectionViewCell.swift
+//  BVSwiftDemo
+//
+//  Created by Balkrishna Singbal on 04/06/20.
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+//
+
+import UIKit
+
+class CurationsCollectionViewCell: UICollectionViewCell {
+    
+}
