f095731e10182943f9ad6990bd106ba506747a1e
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-06-05 11:57:44 +0530

    Recommendations Cell design.

---
+++ Examples/BVSwiftDemo/BVSwiftDemo/Views/ProductRecommendationsCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductRecommendationsCell.swift
+//  BVSwiftDemo
+//
+//  Created by Balkrishna Singbal on 05/06/20.
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+//
+
+import UIKit
+
+class ProductRecommendationsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
