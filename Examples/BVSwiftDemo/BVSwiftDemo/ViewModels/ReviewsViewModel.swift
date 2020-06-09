51ce75eda0c0c80979715a6ab380aab7e501e680
Abhinav Mandloi <abhinav.mandloi@bazaarvoice.com>
2020-06-09 17:10:26 +0530

    Added UI class and Views.

---
+++ Examples/BVSwiftDemo/BVSwiftDemo/ViewModels/ReviewsViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  ReviewsViewModel.swift
+//  BVSwiftDemo
+//
+//  Created by Abhinav Mandloi on 09/06/2020.
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+//
+
+import UIKit
+
+class ReviewsViewModel: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
