d3fe005b8c9ce396ea84c7673e11089067a9d810
Abhinav Mandloi <abhinav.mandloi@bazaarvoice.com>
2020-07-10 11:19:35 +0530

    Added new cell for Advertising.

---
+++ Examples/BVSwiftDemo/BVSwiftDemo/Views/UICollectionViewCells/HomeAdvertisementCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HomeAdvertisementCollectionViewCell.swift
+//  BVSwiftDemo
+//
+//  Created by Abhinav Mandloi on 09/07/2020.
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+//
+
+import UIKit
+
+class HomeAdvertisementCollectionViewCell: UICollectionViewCell {
+    
+}
