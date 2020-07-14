148649e9910526dd73dc846bfa4a7c80649b5dd6
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-07-14 12:37:15 +0530

    Created ViewController, ViewModel classes for Ask A Question screen.

---
+++ Examples/BVSwiftDemo/BVSwiftDemo/ViewModels/AskAQuestionViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  AskAQuestionViewModel.swift
+//  BVSwiftDemo
+//
+//  Created by Balkrishna Singbal on 14/07/20.
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+//
+
+import Foundation
+
+protocol AskAQuestionViewModelDelegate: class {
+  
+}
+
+class AskAQuestionViewModel: ViewModelType {
+  
+  weak var viewController: AskAQuestionViewControllerDelegate?
+  
+  weak var coordinator: Coordinator?
+    
+  
+}
