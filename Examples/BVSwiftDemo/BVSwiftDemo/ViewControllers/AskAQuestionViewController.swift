dfbe0c52737fbfc9fb4ce522cf521050b50b07d2
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-07-14 13:06:56 +0530

    Created IBOutlets for header view.

--- Examples/BVSwiftDemo/BVSwiftDemo/ViewControllers/AskAQuestionViewController.swift
+++ Examples/BVSwiftDemo/BVSwiftDemo/ViewControllers/AskAQuestionViewController.swift
@@ -7,36 +7,51 @@
 //
 
 import UIKit
+import HCSStarRatingView
 
 protocol AskAQuestionViewControllerDelegate: class {
-  
+    
 }
 
 class AskAQuestionViewController: UIViewController, ViewControllerType {
-  
-  // MARK:- Variables
-  var viewModel: AskAQuestionViewModelDelegate!
-
+    
+    // MARK:- Variables
+    var viewModel: AskAQuestionViewModelDelegate!
+    
+    // MARK:- IBOutlets
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productStarRatingView: HCSStarRatingView!
+    
+    // MARK:- Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Submit",
+                                                                 style: .plain,
+                                                                 target: self,
+                                                                 action: #selector(AskAQuestionViewController.submitQuestionTapped))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func submitQuestionTapped() {
+        self.viewModel.submitQuestionTapped()
     }
-    */
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
 
 // MARK:- AskAQuestionViewControllerDelegate methods
 extension AskAQuestionViewController: AskAQuestionViewControllerDelegate {
-  
+    
 }
