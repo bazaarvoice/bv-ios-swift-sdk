00fda177c91314aff1f78b1fc39bee3c3f8de1b8
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-09-28 12:38:14 +0530

    Created BVProductsQueryTest class. Added test cases for filteredReviewStats query param.

---
+++ BVSwiftTests/BVConversations/Display/BVProductsQueryTest.swift
@@ -0,0 +1,88 @@
+//
+//
+//  BVProductsQueryTest.swift
+//  BVSwift
+//
+//  Copyright © 2020 Bazaarvoice. All rights reserved.
+// 
+
+import XCTest
+@testable import BVSwift
+
+class BVProductsQueryTest: XCTestCase {
+  
+  private static var config: BVConversationsConfiguration =
+  { () -> BVConversationsConfiguration in
+    
+    let analyticsConfig: BVAnalyticsConfiguration =
+      .dryRun(
+        configType: .staging(clientId: "apitestcustomer"))
+    
+    return BVConversationsConfiguration.display(
+      clientKey: "kuy3zj9pr3n7i0wxajrzj04xo",
+      configType: .staging(clientId: "apitestcustomer"),
+      analyticsConfig: analyticsConfig)
+  }()
+  
+  private static var privateSession:URLSession = {
+    return URLSession(configuration: .default)
+  }()
+  
+  override func setUp() {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+    
+  func testProductsQueryIncentivizedStats() {
+    let expectation = self.expectation(description: "testProductsQueryIncentivizedStats")
+    
+    let productQuery = BVProductsQuery(productIds: ["test1", "test2"])!
+      .stats(.reviews)
+      .filter(.reviews)
+      .configure(BVProductsQueryTest.config)
+      .handler { (response: BVConversationsQueryResponse<BVProduct>) in
+        
+        if case .failure(let error) = response {
+          print(error)
+          XCTFail()
+          expectation.fulfill()
+          return
+        }
+        
+        guard case let .success(_, products) = response else {
+          XCTFail()
+          expectation.fulfill()
+          return
+        }
+        
+        XCTAssertEqual(products.count, 2)
+        
+        for product in products {
+          XCTAssertNotNil(product.productId)
+          XCTAssertNotNil(product.reviewStatistics)
+          XCTAssertNotNil(product.filteredReviewStatistics)
+        }
+        
+        expectation.fulfill()
+    }
+    
+    guard let req = productQuery.request else {
+      XCTFail()
+      expectation.fulfill()
+      return
+    }
+    
+    print(req)
+    
+    /// We're not testing analytics here
+    productQuery.async(urlSession: BVProductsQueryTest.privateSession)
+    
+    self.waitForExpectations(timeout: 20) { (error) in
+      XCTAssertNil(
+        error, "Something went horribly wrong, request took too long.")
+    }
+  }
+}
