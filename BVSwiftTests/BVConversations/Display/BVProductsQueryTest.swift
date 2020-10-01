2076540787fd64bdce6b9586e20f751e47badc91
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2020-10-01 10:46:55 +0530

    Incentivized Stats test case implementation.

--- BVSwiftTests/BVConversations/Display/BVProductsQueryTest.swift
+++ BVSwiftTests/BVConversations/Display/BVProductsQueryTest.swift
@@ -24,6 +24,19 @@ class BVProductsQueryTest: XCTestCase {
       analyticsConfig: analyticsConfig)
   }()
   
+  private static var incentivizedStatsConfig: BVConversationsConfiguration =
+  { () -> BVConversationsConfiguration in
+    
+    let analyticsConfig: BVAnalyticsConfiguration =
+      .dryRun(
+        configType: .staging(clientId: "apitestcustomer"))
+    
+    return BVConversationsConfiguration.display(
+      clientKey: "caB45h2jBqXFw1OE043qoMBD1gJC8EwFNCjktzgwncXY4",
+      configType: .staging(clientId: "apitestcustomer"),
+      analyticsConfig: analyticsConfig)
+  }()
+  
   private static var privateSession:URLSession = {
     return URLSession(configuration: .default)
   }()
@@ -88,5 +101,85 @@ class BVProductsQueryTest: XCTestCase {
       XCTAssertNil(
         error, "Something went horribly wrong, request took too long.")
     }
+  }
+  
+  func testProductsQueryIncentivizedStats() {
+    let expectation = self.expectation(description: "testProductsQueryIncentivizedStats")
+        
+    let productQuery = BVProductsQuery(productIds: ["test1234567", "wsd013c"])!
+      .stats(.reviews)
+      .filter(.reviews)
+      .incentivizedStats(true)
+      .configure(BVProductsQueryTest.incentivizedStatsConfig)
+      .handler { (response: BVConversationsQueryResponse<BVProduct>) in
+        
+        if case .failure(let error) = response {
+          print(error)
+          XCTFail()
+          expectation.fulfill()
+          return
+        }
+        
+        guard case let .success(_, products) = response else {
+          XCTFail()
+          expectation.fulfill()
+          return
+        }
+        
+        for product in products {
+          XCTAssertNotNil(product.productId)
+          XCTAssertNotNil(product.reviewStatistics)
+          XCTAssertNotNil(product.reviewStatistics?.incentivizedReviewCount)
+        }
+        
+        guard let product: BVProduct = products.first else {
+            XCTFail()
+            expectation.fulfill()
+            return
+        }
+        
+        XCTAssertEqual(product.productId, "wsd013c")
+        
+        // Review Statistics assertions
+        XCTAssertNotNil(product.reviewStatistics)
+        XCTAssertNotNil(product.reviewStatistics?.incentivizedReviewCount)
+        XCTAssertEqual(product.reviewStatistics?.incentivizedReviewCount, 1)
+        XCTAssertNotNil(product.reviewStatistics?.contextDataDistribution?.first(where: { $0.distibutionElementId == "IncentivizedReview" }))
+        
+        let incentivizedReview = product.reviewStatistics?.contextDataDistribution?.first(where: { $0.distibutionElementId == "IncentivizedReview" })!
+        XCTAssertEqual(incentivizedReview?.distibutionElementId, "IncentivizedReview")
+        XCTAssertEqual(incentivizedReview?.label, "Received an incentive for this review")
+        XCTAssertEqual(incentivizedReview?.values?.count, 1)
+        
+        // Filtered Review Statistics assertions
+        XCTAssertNotNil(product.filteredReviewStatistics)
+        XCTAssertNotNil(product.filteredReviewStatistics?.incentivizedReviewCount)
+        XCTAssertEqual(product.filteredReviewStatistics?.incentivizedReviewCount, 1)
+        XCTAssertNotNil(product.filteredReviewStatistics?.contextDataDistribution?.first(where: { $0.distibutionElementId == "IncentivizedReview" }))
+        
+        let filtereedIncentivizedReview = product.filteredReviewStatistics?.contextDataDistribution?.first(where: { $0.distibutionElementId == "IncentivizedReview" })!
+        XCTAssertEqual(filtereedIncentivizedReview?.distibutionElementId, "IncentivizedReview")
+        XCTAssertEqual(filtereedIncentivizedReview?.label, "Received an incentive for this review")
+        XCTAssertEqual(filtereedIncentivizedReview?.values?.count, 1)
+        
+        expectation.fulfill()
+    }
+    
+    guard let req = productQuery.request else {
+      XCTFail()
+      expectation.fulfill()
+      return
+    }
+    
+    print(req)
+    
+    /// We're not testing analytics here
+    productQuery.async(urlSession: BVProductsQueryTest.privateSession)
+    
+    self.waitForExpectations(timeout: 20) { (error) in
+      XCTAssertNil(
+        error, "Something went horribly wrong, request took too long.")
+    }
+    
   }
 }
