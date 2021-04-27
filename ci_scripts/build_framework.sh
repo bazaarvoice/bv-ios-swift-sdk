72e17afc58941031bb5b9eb96f122c149c0f3872
Balkrishna Singbal <balkrishna.singbal@bazaarvoice.com>
2021-04-27 15:13:44 +0530

    UseModernBuildSystem flag set to NO.

--- ci_scripts/build_framework.sh
+++ ci_scripts/build_framework.sh
@@ -2,7 +2,7 @@ echo Starting run_tests script
 set -euf -o pipefail
 
 echo Building universal framework
-xcodebuild -project BVSwift.xcodeproj -scheme BVSwift-Universal -configuration Release clean build | xcpretty -c
+xcodebuild -project BVSwift.xcodeproj -scheme BVSwift-Universal -configuration Release clean build -UseModernBuildSystem=NO | xcpretty -c
 
 echo zipping framework...
 ditto -ck --rsrc --sequesterRsrc --keepParent ./Output/BVSwift-Release-iphoneuniversal/BVSwift.framework BVSwift.framework.zip
