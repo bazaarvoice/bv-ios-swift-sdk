93711c95248b4b750cd2f9486f229e09b9e1edf3
chetan shanbag <chetan.shanbag@bazaarvoice.com>
2022-01-21 19:49:47 +0530

    Release 1.9.0

--- BVSwift/BVCommon/Configuration/BVConstants.swift
+++ BVSwift/BVCommon/Configuration/BVConstants.swift
@@ -18,7 +18,7 @@ internal struct BVConstants {
     static let appVersionField: String = "_appVersion"
     static let buildNumberField: String = "_buildNumber"
     static let sdkVersionField: String = "_bvIosSwiftSdkVersion"
-    static let bvSwiftSDKVersion: String = "1.8.0"
+    static let bvSwiftSDKVersion: String = "1.9.0"
     
 }
 
